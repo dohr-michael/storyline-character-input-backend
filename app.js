b009770bb510f79f262a7ebdd5231b4524bcd9c6
michael <dohr.michael@gmail.com>
2014-09-14 14:55:46 +0200

    Access control allow origin

--- app.js
+++ app.js
@@ -29,6 +29,13 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
+// Other configurations
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+});
+
 // Application routes
 routes(app);
 
